60626217a8fcb65a4cc3a269d47e6ca562a444f6
Kent B Carlyle II <kentbcarlyle@gmail.com>
2019-06-02 18:56:03 -0400

    add jumbotron, cards, and practice margin and grid sys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    JumbotronComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule
